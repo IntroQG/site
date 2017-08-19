1cdda19eebf4dbaeb1cc3def88655f6545289ee5
Dave Whipp <david_d0t_whipp@helsinki.fi>
2017-08-19 23:42:07 +0300

    Added option for ipython formatted output inline

--- source/conf.py
+++ source/conf.py
@@ -32,7 +32,10 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.mathjax',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages',
+    'IPython.sphinxext.ipython_console_highlighting',
+    'IPython.sphinxext.ipython_directive'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
