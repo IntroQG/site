1ec1c3fbd90c042edcd2c1507c7aa037e0e0c3de
Dave Whipp <davewhipp@users.noreply.github.com>
2019-10-22 11:12:28 +0300

    Build only master, develop for now

--- source/conf.py
+++ source/conf.py
@@ -221,4 +221,5 @@ nbsphinx_allow_errors = True
 
 # Sphinx versioning settings
 scv_show_banner = True
-scv_whitelist_branches = ('master', 'develop', '2017')
+#scv_whitelist_branches = ('master', 'develop', '2017')
+scv_whitelist_branches = ('master', 'develop')
