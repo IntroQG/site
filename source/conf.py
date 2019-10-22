b420e4ec1963a5c6d7318ff5766530c7ed8099a0
Dave Whipp <davewhipp@users.noreply.github.com>
2019-10-22 12:34:00 +0300

    Remove extra line related to RTD theme

--- source/conf.py
+++ source/conf.py
@@ -88,7 +88,6 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_logo = 'img/HY-logo.png'
 
